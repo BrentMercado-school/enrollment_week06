b0a10193bd2a20a9209bb5a4eae6ff2e95f8672b
Brent Mercado <brentallen.mercado@benilde.edu.ph>
2025-11-13 08:19:34 +0800

    final commit

--- enrollment.py
+++ enrollment.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+enrollment = {
+    "IT101": {'Alice', 'Bob', 'Charlie'},
+    "CS102": {'Bob', 'Diana'},
+    "IS201": {'Charlie', 'Eve'}
+}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("Current Enrollment:")
+for course in enrollment:
+    print(f"{course}: {enrollment[course]}")
+
+print("\nAdding student 'Frank' to CS102...")
+enrollment["CS102"].add('Frank')
+print("Removing student 'Alice' from IT101...")
+enrollment["IT101"].discard("Alice")
+
+print("\nUpdated Enrollment:")
+for course in enrollment:
+    print(f"{course}: {enrollment[course]}")
+
+count_courses = {}
+for course, students in enrollment.items():
+    for student in students:
+        count_courses[student] = count_courses.get(student, 0) + 1
+
+student_multiple = {student for student, count in count_courses.items() if count > 1}
+print(f"\nStudent enrolled in multiple courses: {student_multiple}")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+unique_student = {student for student in count_courses}
+print(f"\nAll unique students: {unique_student}")
